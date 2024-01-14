4a05d707e663874d0ccc7ebd04442d4c2a6165e8
yamazaki <1521ttyy@gmail.com>
2024-01-14 18:03:41 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           HELLO WORLD
+           HELLO WORLD2
         </p>
       </header>
     </div>
